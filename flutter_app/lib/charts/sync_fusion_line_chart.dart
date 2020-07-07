f4b3612a22941f8731288cbe1499e1e8bf34434b
rosetta <rosetta3327@gmail.com>
2020-07-07 16:41:24 +0800

    update syncfusion lib demo

--- flutter_app/lib/charts/sync_fusion_line_chart.dart
+++ flutter_app/lib/charts/sync_fusion_line_chart.dart
@@ -8,11 +8,11 @@ class SyncFusionLineChart extends StatelessWidget {
         appBar: AppBar(title: Text('line chart')),
         body: SizedBox(
           height: 500,
-          child: getSfchart(),
+          child: getSfChart(),
         ));
   }
 
-  getSfchart() {
+  getSfChart() {
     return SfCartesianChart(
         primaryXAxis: CategoryAxis(),
         // Chart title
@@ -26,8 +26,24 @@ class SyncFusionLineChart extends StatelessWidget {
             enablePanning: true,
             // enableMouseWheelZooming: true,
             zoomMode: ZoomMode.x),
+        crosshairBehavior: CrosshairBehavior(
+          lineType: CrosshairLineType.horizontal, //横向选择指示器
+          enable: true,
+          shouldAlwaysShow: false, //十字架始终显示(横向选择指示器)
+          activationMode: ActivationMode.singleTap,
+        ),
+        trackballBehavior: TrackballBehavior(
+          lineType: TrackballLineType.vertical, //纵向选择指示器
+          activationMode: ActivationMode.singleTap,
+          enable: true,
+          tooltipAlignment: ChartAlignment.near,
+          shouldAlwaysShow: true, //跟踪球始终显示(纵向选择指示器)
+          tooltipDisplayMode:
+              TrackballDisplayMode.groupAllPoints, //工具提示模式(全部分组)
+        ),
         series: <ChartSeries<SalesData, String>>[
           LineSeries<SalesData, String>(
+              name: 'first',
               dataSource: <SalesData>[
                 SalesData('Jan', 35),
                 SalesData('Feb', 28),
@@ -35,11 +51,14 @@ class SyncFusionLineChart extends StatelessWidget {
                 SalesData('Apr', 32),
                 SalesData('May', 40)
               ],
+              dashArray: [10, 3, 3, 3],
               xValueMapper: (SalesData sales, _) => sales.year,
               yValueMapper: (SalesData sales, _) => sales.sales,
+              markerSettings: MarkerSettings(isVisible: true),
               // Enable data label
               dataLabelSettings: DataLabelSettings(isVisible: true)),
-          LineSeries<SalesData, String>(
+          SplineSeries<SalesData, String>(
+              name: 'second',
               dataSource: <SalesData>[
                 SalesData('Jan', 125),
                 SalesData('Feb', 18),
@@ -50,7 +69,19 @@ class SyncFusionLineChart extends StatelessWidget {
               xValueMapper: (SalesData sales, _) => sales.year,
               yValueMapper: (SalesData sales, _) => sales.sales,
               // Enable data label
-              dataLabelSettings: DataLabelSettings(isVisible: true))
+              dataLabelSettings: DataLabelSettings(isVisible: true)),
+          SplineAreaSeries<SalesData, String>(
+              name: 'area',
+              dataSource: <SalesData>[
+                SalesData('Jan', 65),
+                SalesData('Feb', 28),
+                SalesData('Mar', 54),
+                SalesData('Apr', 22),
+                SalesData('May', 10)
+              ],
+              opacity: 0.1,
+              xValueMapper: (SalesData sales, _) => sales.year,
+              yValueMapper: (SalesData sales, _) => sales.sales,)
         ]);
   }
 }
